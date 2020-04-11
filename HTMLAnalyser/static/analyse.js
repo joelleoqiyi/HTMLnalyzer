1a8728fcd43d9f24f102bd1b8f43b57ecc70bb64
kaslisk <61258273+kaslisk@users.noreply.github.com>
2020-04-11 23:26:15 +0800

    Create analyse.js

---
+++ HTMLAnalyser/static/analyse.js
@@ -0,0 +1,5 @@
+    var analyse_form = document.getElementById('analyse_form');
+    var analyse_button = document.getElementById('analyse_button');
+    analyse_button.addEventListener('click', event => {
+        analyse_form.submit();
+    });
